81d6fd5e7b460eab9d492bf765bbd430cb96dc28
IchbineineBiene2 <yukselahmett501@gmail.com>
2024-08-26 20:25:05 +0300

    Connected PHP to PostgreSQL and displayed product data

---
+++ script.js
@@ -0,0 +1,9 @@
+// Sayfa yüklendiğinde bir mesaj göstermek için
+window.onload = function() {
+    alert("Welcome to My E-Commerce Site!");
+};
+
+// Bir butona tıklanınca bir mesaj göstermek için
+function showMessage() {
+    alert("Button clicked!");
+}
